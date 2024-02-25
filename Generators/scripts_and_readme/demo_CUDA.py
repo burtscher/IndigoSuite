271a4882e7cb13d12db8d0a849a6207e63032566
burtscher@txstate.edu <burtscher@txstate.edu>
2024-02-25 07:21:19 -0600

    version 1.2

--- Generators/scripts_and_readme/demo_CUDA.py
+++ Generators/scripts_and_readme/demo_CUDA.py
@@ -8,17 +8,17 @@ if (len(args) != 3):
     sys.exit('USAGE: threads_per_block number_of_blocks\n')
 
 # test a bug-free microbenchmark
-print('atomic_count.cu')
+print('conditional_vertex_neighbor.cu')
 print('undirect_power_law_50n_400e.egr')
-os.system('nvcc -Iinclude CUDA/atomic_count/atomic_count.cu -o microbenchmark')
+os.system('nvcc -Iinclude CUDA/conditional_vertex_neighbor/conditional_vertex_neighbor.cu -o microbenchmark')
 os.system('cuda-memcheck --tool memcheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool initcheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool synccheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
 
 # test a microbenchmark that has atomic bug
-print('atomic_count_atomicBug.cu')
+print('conditional_vertex_neighbor_atomicBug.cu')
 print('undirect_power_law_50n_400e.egr')
-os.system('nvcc -Iinclude CUDA/atomic_count/atomic_count_atomicBug.cu -o microbenchmark')
+os.system('nvcc -Iinclude CUDA/conditional_vertex_neighbor/conditional_vertex_neighbor_atomicBug.cu -o microbenchmark')
 os.system('cuda-memcheck --tool memcheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool initcheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool synccheck ./microbenchmark inputs/power_law/undirect_power_law_50n_400e.egr %s %s' % (str(args[1]), str(args[2])))
@@ -26,9 +26,9 @@ os.system('cuda-memcheck --tool synccheck ./microbenchmark inputs/power_law/undi
 
 # test a microbenchmark that has memory bound bug
 # it may or may not cause out-of-bound memory access depending on the input
-print('atomic_count_neighbor_cond_boundsBug2.cu')
+print('conditional_vertex_neighbor_boundsBug.cu')
 print('1dim_grid_31n_30e.egr')
-os.system('nvcc -Iinclude CUDA/atomic_count_neighbor_cond/atomic_count_neighbor_cond_boundsBug2.cu -o microbenchmark')
+os.system('nvcc -Iinclude CUDA/conditional_vertex_neighbor/conditional_vertex_neighbor_boundsBug.cu -o microbenchmark')
 os.system('cuda-memcheck --tool memcheck ./microbenchmark inputs/k_dim_grid/1dim_grid_31n_30e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool initcheck ./microbenchmark inputs/k_dim_grid/1dim_grid_31n_30e.egr %s %s' % (str(args[1]), str(args[2])))
 os.system('cuda-memcheck --tool synccheck ./microbenchmark inputs/k_dim_grid/1dim_grid_31n_30e.egr %s %s' % (str(args[1]), str(args[2])))
